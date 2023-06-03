e5cc18c83800c8c1a4d0bce99ddacd0f947bf298
lixvbnet <lixvbnet@gmail.com>
2023-06-03 11:21:17 +0800

    enhance build script

--- Makefile
+++ Makefile
@@ -1,18 +1,19 @@
 DIST_DIR	= dist
 NAME 		= $(shell basename $$PWD)
+MAIN_DIR	= .
 LD_FLAGS	= -X 'main.Name=$(NAME)' -X 'main.GitHash=`git rev-parse --short=8 HEAD`'
 
 build:
-	go build -ldflags="$(LD_FLAGS)"
+	go build -ldflags="$(LD_FLAGS)"	$(MAIN_DIR)
 
 install:
-	go install -ldflags="$(LD_FLAGS)"
+	go install -ldflags="$(LD_FLAGS)" $(MAIN_DIR)
 
 test:
 	cd sshlib && go test -v
 
 package: clean test
-	sh build.sh $(DIST_DIR) $(NAME) "-ldflags=\"$(LD_FLAGS)\""
+	sh build.sh $(DIST_DIR) $(NAME) "-ldflags=\"$(LD_FLAGS)\"" $(MAIN_DIR)
 
 
 clean:
--- build.sh
+++ build.sh
@@ -9,12 +9,16 @@ buildTarget() {
   flags=$5
 
   # grep line 'const Version = ""'
-  version=$(grep -E "const\b\s*\bVersion\s*=" main.go | grep -E -o "(\d+)((\.{1}\d+)*)(\.{0})")
+  version=$(grep -E "const\b\s*\bVersion\s*=" "$main_dir/main.go" | grep -E -o "(\d+)((\.{1}\d+)*)(\.{0})")
   folder=$name-$version-$os-$arch
   folderPath=$dist_dir/$folder
 
   echo "---------------------------------------------"
-  build_cmd="go build -o $folderPath/$name $flags"
+  filename=$name
+  if [ "$os" = "windows" ]; then
+    filename="$name.exe"
+  fi
+  build_cmd="go build -o $folderPath/$filename $flags $main_dir"
   echo "$build_cmd"
   env GOOS="$os" GOARCH="$arch" sh -c "$build_cmd"
   tar -zcf "$folderPath.tar.gz" -C "$dist_dir" "$folder"
@@ -23,14 +27,17 @@ buildTarget() {
 #dist_dir=dist
 #name=$(basename $PWD)
 #flags="-ldflags=\"-X 'main.CMD=$name'\""
+#main_dir="./cmd/$name"
+#main_dir="."
 
 dist_dir=$1
 name=$2
 flags=$3
+main_dir=$4
 
 
 for os in "${OS_LIST[@]}"; do
   for arch in "${ARCH_LIST[@]}"; do
-    buildTarget "$dist_dir" "$name" "$os" "$arch" "$flags"
+    buildTarget "$dist_dir" "$name" "$os" "$arch" "$flags" "$main_dir"
   done
 done
